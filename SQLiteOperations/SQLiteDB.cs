9b3e22a4aa6af5f3636dc2f63119556a654d23b3
eyupdalan <eyupdalan@gmail.com>
2015-05-23 22:11:03 +0300

    add

---
+++ SQLiteOperations/SQLiteDB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLiteOperations
+{
+    public class SQLiteDB
+    {
+
+        private string dbPath { get; set; }
+
+        public SQLiteDB(string dbPath)
+        {
+            this.dbPath = dbPath;
+        }
+
+
+    }
+}
